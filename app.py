4033832968ac2e8d7289030bba0df63a3c960714
John Lynch <johnlynch.32768@gmail.com>
2019-01-21 23:51:32 +0000

    Set up route, jinja template, view of all veg records

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
-
+from flask import Flask, request, render_template, redirect, url_for
+from flask_pymongo import flask_pymongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "wc-veg"
+app.config["MONGO_URI"] = "mongodb://<dbuser>:<dbpassword>@ds163054.mlab.com:63054/wc-veg"
+
+mongo = bPyMongo(app)
 
 @app.route("/")
-def hello():
-	return "Hello Star Cluster"
+@app.route("/get_veg")
+def get_veg():
+	return render_template("veg.html", veg = mongo.db.vegetables.find())
+
+
+# @app.route("/")
+# def hello():
+# 	return "Hello Star Cluster"
 
 if __name__ == "__main__":
 	app.run(host = os.environ.get('IP', "0.0.0.0"),
