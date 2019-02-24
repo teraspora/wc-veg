1908ca5d3370e706c4810512fc2c3f036ed67753
John Lynch <johnlynch.32768@gmail.com>
2019-02-24 02:06:08 +0000

    Added basic user tracking and login form

--- app.py
+++ app.py
@@ -1,25 +1,74 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import sys
+
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET", "12676506002A2822HOD940149670../")
 app.config["MONGO_DBNAME"] = "wc-veg"
 app.config["MONGO_URI"] = "mongodb://tsadm:MDBpw256!@ds163054.mlab.com:63054/wc-veg"
 
 mongo = PyMongo(app)
 
-@app.route("/")
-@app.route("/get_veg")
+# Keep a list of known users
+users = []    
+user = ''
+userid = -1
+
+class User:
+    """ User class represents a user and associated data """
+    def __init__(self, name = "anon", admin = False):
+        """ Create a user """
+        self.name = name
+        self.admin = admin
+
+
+
+
+
+
+
+
+@app.route("/get_veg", methods = ["GET", "POST"]) 
 def get_veg():
-	return render_template("veg.html", veg = mongo.db.vegetables.find())
+    if request.method == "GET" and (request.args.get("logout_button")  or session.get("userid", None) is None):
+        session["userid"] = -1
+        return redirect(url_for("index"))        
+        
+    if request.method == "POST":
+        print(dict(request.form), file=sys.stdout)
+        uname = request.form["uname"]
+        userid = next((i for i, user in enumerate(users) if user.name == uname), -1)
+        
+        if userid == -1:    # so it's a new user
+            userid = len(users)
+            user = User(uname)
+            users.append(user)
+        else:               # so we already know this user
+            user = users[userid]
+            
+        session["userid"] = userid # save userid on client
+    
+        # if coming from "Next problem" button in mark.html:
+        userid = session.get("userid", None)
+        user = users[userid]
+    
+    return render_template("veg.html", veg = mongo.db.vegetables.find(), uname = user.name)
+
+@app.route("/")
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
-# @app.route("/")
-# def hello():
-# 	return "Hello Star Cluster"
+
+
+
+
 
 if __name__ == "__main__":
-	app.run(host = os.environ.get('IP', "0.0.0.0"),
+    app.run(host = os.environ.get('IP', "0.0.0.0"),
             port = int(os.environ.get('PORT', "5000")),
             debug = True)
