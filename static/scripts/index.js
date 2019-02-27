34fde608d765c67e2ebe61c2b31290111124a431
John Lynch <johnlynch.32768@gmail.com>
2019-02-27 05:02:34 +0000

    Attempted to get mobile nav working; currently buggy.

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,9 +1,14 @@
 // Activation functions for Materialize
 
 $(document).ready(function(){
-
+  console.log("running jQuery activators...");
   // Activate side navigation for small screens:
   $('.sidenav').sidenav();
+
+  // $('.button-collapse').sideNav({
+  //     closeOnClick: true
+  //   }
+  // );
 
   // // Push alternate rows around:
   // $('.section .row:nth-of-type(2n+1) .col:last-of-type').addClass("offset-l1");
@@ -13,7 +18,7 @@ $(document).ready(function(){
   // $('.section .row:nth-of-type(2n) .col:last-of-type').addClass("pull-l5 right-align offset-l1");
 
   // // Make image zoom on click:
-  // $('.materialboxed').materialbox();
+  $('.materialboxed').materialbox();
 
   // // Activate 'parallax' scrolling:
   // $('.parallax').parallax();
@@ -24,5 +29,6 @@ $(document).ready(function(){
   // $('.collapsible').collapsible();
   
   $('select').formSelect();
-
+  console.log("...Done!");
+  
 });
